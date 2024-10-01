d1e8ebf0c7b0852955eaad8bd85e7ed693bb858e
jacksonm <jacksonm@getamplifylife.com>
2024-10-01 14:35:38 -0400

    added comments

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import CollaborativeRoom from "@/components/CollaborativeRoom";
 import { getDocument } from "@/lib/actions/room.actions";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -15,10 +16,35 @@ const Document = async ({ params: { id } }: SearchParamProps) => {
   if (!room) redirect("/");
 
   //TODO assess permission level
+  const userIds = Object.keys(room.usersAccesses);
+  const users = (await getClerkUsers({ userIds })) as User[];
+
+  const usersData = users?.map((user: User) => ({
+    ...user,
+    userType: (room.usersAccesses[user.email] as unknown as string)?.includes(
+      "room:write",
+    )
+      ? "editor"
+      : "viewer",
+  })) as User[];
+
+  const currentUserType = (
+    room.usersAccesses[
+      clerkUser.emailAddresses[0].emailAddress
+    ] as unknown as string
+  )?.includes("room:write")
+    ? "editor"
+    : "viewer";
 
   return (
     <div className="flex w-full items-center">
-      <CollaborativeRoom roomId={id} roomMetadata={room.metadata} />
+      <CollaborativeRoom
+        /* @ts-ignore  */
+        roomMetadata={room.metadata}
+        users={usersData}
+        currentUserType={currentUserType}
+        roomId={id}
+      />
     </div>
   );
 };
